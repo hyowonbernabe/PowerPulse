8fbeff90c135f572ebbc185d88be142765b7ef3a
HYOWON ARZIL BERNABE <2244644@slu.edu.ph>
2024-12-10 11:58:45 +0800

    add dark mode implementation

--- app/src/main/java/com/example/powerpulse/ui/analytics/AnalyticsViewModel.kt
+++ app/src/main/java/com/example/powerpulse/ui/analytics/AnalyticsViewModel.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.ViewModel
 class AnalyticsViewModel : ViewModel() {
     private val dailyData = listOf(
         1.2f, 1.5f, 1.8f, 2.1f, 1.9f, 1.4f, 1.1f, 1.0f,
-        1.3f, 1.7f, 2.3f, 2.5f, 2.0f, 1.6f, 1.2f, 0f,
-        0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+        1.3f, 1.7f, 2.3f, 2.5f, 2.0f, 1.6f, 1.2f, 3.2f,
+        1.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
     )
 
     private val weeklyData = listOf(
-        12.5f, 0.02f, 0f, 0f, 0f, 0f, 0f
+        12.5f, 9.6f, 0.2f, 0f, 0f, 0f, 0f
     )
 
     private val _barChartData = MutableLiveData<List<Float>>()
